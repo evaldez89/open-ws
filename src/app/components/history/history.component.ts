0bb3e72ccb707650292a5271f1a5956036951fe1
Emmanuel Valdez <--global>
2021-01-23 23:22:24 -0400

    add share functionality

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Log } from 'src/app/interfaces/log';
 import { LocalDataService } from 'src/app/service/local-data.service';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-log-history',
@@ -11,14 +13,37 @@ export class HistoryComponent implements OnInit {
 
   historyRecords: Log[];
 
-  constructor(private localData: LocalDataService) { }
+  constructor(private socialSharing: SocialSharing,
+              private platform: Platform,
+              private localData: LocalDataService) { }
 
   ngOnInit() {
     this.loadHistory();
   }
 
   async loadHistory(){
-    this.historyRecords = (await this.localData.loadHistory()) || [];
+    this.historyRecords = (await this.localData.loadHistory());
+  }
+
+  async share(item: Log){
+    let url = `${await this.localData.wsApiUrl}${item.phoneNumber}`;
+
+    url = url.replace('whatsapp://', '')
+
+    const options = {
+      message: `Chat con Número: *${item.phoneNumber}*\n`,
+      subject: "Compartir Número de Teléfono",
+      url
+    };
+
+    if(this.platform.is('capacitor') || this.platform.is('cordova')) {
+      this.socialSharing.shareWithOptions(options);
+      // this.socialSharing.shareWithOptions(options).then(resp => {console.log('success'); })
+    } else if (navigator['share']) {
+      navigator['share'](options)
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+    }
   }
 
   deleteAll(){
